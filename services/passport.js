d831b28b74b8664247ac5af5108474a6762b0dea
YanZhi <YanZhi@YanZhi-MacBook-Pro.local>
2018-07-22 10:40:56 +0800

    tweak google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -23,7 +23,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true //解決http and https的問題
     },
     (accessToken, refreshToken, profile, done) => {
       //console.log('access token', accessToken);
